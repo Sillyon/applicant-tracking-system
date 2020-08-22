81ac8d8a8185531ea7649c82ffae6cc52a6841b2
Sillyon <comakfatihh@gmail.com>
2020-08-23 00:55:19 +0300

    init commit

---
+++ src/test/java/com/fiz/demo/ApplicantTrackingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fiz.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicantTrackingSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
